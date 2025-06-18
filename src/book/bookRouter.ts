894dfd013ba8a56c2f0cb37bfc4e59cb8165f9b1
Sandeep <sandeepwadhawan2001@gmail.com>
2025-06-19 01:39:23 +0530

    Setup multer

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,8 +1,29 @@
 import express from 'express';
 import { createBook } from './bookController';
+import multer from 'multer';
+import path from 'node:path';
 
 const bookRouter = express.Router();
 
-bookRouter.post('/create', createBook);
+// Multer is a node.js middleware for handling multipart/form-data, which is primarily used for uploading files. 
+
+// Working: Locally file store karta hai multer file system mein and then fir baad mein cloudinary pe upload krta hai yahan se utha kar and finally iss local system se delete kar deta hai
+const upload = multer({
+    dest: path.resolve(__dirname, "../../public/data/uploads"),
+    limits: {
+        fileSize: 3e7
+    },
+});
+
+bookRouter.post('/create', upload.fields([
+    {
+        name: "coverImage",
+        maxCount: 1
+    },
+    {
+        name: "file",
+        maxCount: 1
+    }
+]), createBook);
 
 export default bookRouter;
