0c68f800d13d27cb53f11d37deb47092c281121c
Sandeep <sandeepwadhawan2001@gmail.com>
2025-06-19 03:12:54 +0530

    Upload files to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,7 +1,45 @@
 import { NextFunction, Request, Response } from "express";
+import cloudinary from "../config/cloudinary";
+import path from "node:path";
+import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-    res.status(201).json({ message: "Book created!!" });
+    console.log("Files", req.files);
+
+    try {
+        const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+
+        const coverImageMimeType = files.coverImage[0].mimetype.split('/').pop();
+        const fileName = files.coverImage[0].filename;
+        const filePath = path.resolve(__dirname, "../../public/data/uploads", fileName);
+
+        // Upload an image
+        const uploadResult = await cloudinary.uploader.upload(filePath, {
+            filename_override: fileName,
+            folder: "book-covers",
+            format: coverImageMimeType
+        });
+
+        const bookFileMimeType = files.file[0].mimetype.split('/').pop();
+        const bookFileName = files.file[0].filename;
+        const bookFilePath = path.resolve(__dirname, "../../public/data/uploads", bookFileName);
+
+        const bookFileUploadResult = await cloudinary.uploader.upload(bookFilePath, {
+            resource_type: "raw",
+            filename_override: bookFileName,
+            folder: "book-pdfs",
+            format: bookFileMimeType
+        })
+
+        console.log("BookFile Upload result", bookFileUploadResult);
+
+        console.log("Uploaded result", uploadResult);
+    
+        res.status(201).json({ message: "Book created!!" });
+    } catch (error) {
+        console.log(error);
+        return next(createHttpError(500, "Error while uploading the files."));
+    }
 };
 
 export { createBook };
--- src/config/config.ts
+++ src/config/config.ts
@@ -9,6 +9,9 @@ const _config = {
     databaseUrl: process.env.MONGO_CONNECTION_STRING,
     env: process.env.NODE_ENV,
     jwtSecret: process.env.JWT_SECRET,
+    cloudinaryCloud: process.env.CLOUDINARY_CLOUD,
+    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+    cloudinarySecret: process.env.CLOUDINARY_API_SECRET
 };
 
 export const config = Object.freeze(_config); // TO make config file read only
